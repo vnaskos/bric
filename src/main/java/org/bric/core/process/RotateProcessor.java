a6e1bf84d710eeac287cb657742c3c5616723811
Vasilis Naskos <vnaskos@gmail.com>
2024-07-21 03:59:48 +0200

    Reuse Random

--- src/main/java/org/bric/core/process/RotateProcessor.java
+++ src/main/java/org/bric/core/process/RotateProcessor.java
@@ -9,6 +9,8 @@ import java.util.Random;
 
 public class RotateProcessor extends ImageProcessor<RotateParameters> {
 
+    private static final Random RANDOM = new Random();
+
     public RotateProcessor(RotateParameters params) {
         super(params);
     }
@@ -26,7 +28,7 @@ public class RotateProcessor extends ImageProcessor<RotateParameters> {
     public boolean isEnabled() {
         return params.isEnabled();
     }
-    
+
     private BufferedImage predefinedRotate(BufferedImage image) {
         int action = params.getAction();
         switch(action){
@@ -54,21 +56,20 @@ public class RotateProcessor extends ImageProcessor<RotateParameters> {
     public BufferedImage customRotate(BufferedImage image) {
         int angle = params.getAngle();
         if(params.isDifferentValues()){
-            Random rand = new Random();
             int max = params.getTo();
             int min = params.getFrom();
             if(params.isLimit()) {
-                angle = rand.nextInt(max-min+1)+min;
+                angle = RANDOM.nextInt(max-min+1)+min;
             } else {
-                angle = rand.nextInt(360);
+                angle = RANDOM.nextInt(360);
             }
         }else if(params.isRandom()){
             angle = params.getRandomAngle();
         }
-        
+
         float radAngle = (float) (((360-angle)*Math.PI)/180);
         RotateFilter rotateFilter = new RotateFilter(radAngle);
         return rotateFilter.filter(image, null);
     }
-    
+
 }
